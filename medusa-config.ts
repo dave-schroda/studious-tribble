471c843451ae8d761995bdaaf8c10aae41c36f5d
dave-schroda <130723314+dave-schroda@users.noreply.github.com>
2025-09-01 14:29:42 -0400

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,16 +1,20 @@
 import { loadEnv, defineConfig } from '@medusajs/framework/utils'
-
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+loadEnv(process.env.NODE_ENV || 'production', process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL, // ← add this line
+    workerMode: (process.env.MEDUSA_WORKER_MODE as 'shared'|'server'|'worker') || 'server',
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
+      storeCors: process.env.STORE_CORS || '',
+      adminCors: process.env.ADMIN_CORS || '',
+      authCors: process.env.AUTH_CORS || '',
+      jwtSecret: process.env.JWT_SECRET || 'supersecret',
+      cookieSecret: process.env.COOKIE_SECRET || 'supersecret',
+    },
+  },
+  admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === 'true',
+  },
 })
